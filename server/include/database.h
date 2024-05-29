84eb877a275233bc9ce4c2c36a6c4400666f2540
keleein <1277821861@qq.com>
2024-05-29 22:36:57 +0800

    Usrinfo

--- server/include/database.h
+++ server/include/database.h
@@ -3,12 +3,28 @@
 
 #include <sqlite3.h>
 
+//用户信息
+typedef struct {
+	unsigned short id;
+	char nickname[32];
+	char passwd[32];
+}Usrinfo;
+
+//基本操作：打开数据库
 sqlite3 *open_database(const char *pathname);
 
+//基本操作：执行sql语句
 int exec_database(sqlite3 *data,char *sql);
 
+//基本操作：获取用户表
 int get_data_base(sqlite3 *data,char *sql,char **res,int *row,int *col);
 
+//基本操作：关闭数据库
 int close_database(sqlite3 *data);
 
+//复合操作：添加用户信息
+int data_user_add(sqlite3 *db,Usrinfo data_usr);
+
+//复合操作：删除用户信息
+int data_user_del(sqlite3 *db,unsigned short id);
 #endif
--- server/src/database.c
+++ server/src/database.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include "database.h"
 
+//基本操作：打开数据库
 sqlite3 *open_database(const char *pathname)
 {
 	sqlite3 *db;
@@ -16,6 +17,7 @@ sqlite3 *open_database(const char *pathname)
 	return db;
 }
 
+//基本操作：执行sql语句
 int exec_database(sqlite3 *data,char *sql)
 {
 	char *errmsg;
@@ -29,6 +31,7 @@ int exec_database(sqlite3 *data,char *sql)
 	return 0;	//exec success
 }
 
+//基本操作：获取数据表
 int get_database(sqlite3 *data,char *sql,char **res,int *row,int *col)
 {
 	char *errmsg;
@@ -42,6 +45,7 @@ int get_database(sqlite3 *data,char *sql,char **res,int *row,int *col)
 	return 0;	//get success
 }
 
+//基本操作：关闭数据库
 int close_database(sqlite3 *data)
 {
 	int ret = sqlite3_close(data);
@@ -51,4 +55,30 @@ int close_database(sqlite3 *data)
 		return -1;
 	}
 	return 0;
+}
+
+//复合操作：增加用户信息
+int data_user_add(sqlite3 *db,Usrinfo data_usr)
+{
+	char sql[128] = "0";
+	sprintf(sql,"insert into user values (%d,'%s','%s');",data_usr.id,data_usr.nickname,data_usr.passwd);
+	if(exec_database(db,sql) != 0)
+	{
+		printf("data_user_add : exec_database error!");
+		return -1;
+	}
+	return 0;
+}
+
+//复合操作：删除用户信息
+int data_user_del(sqlite3 *db,unsigned short id)
+{
+	char sql[128] = "0";
+	sprintf(sql,"delete from user where id=%d;",id);
+	if(exec_database(db,sql) != 0)
+        {
+                printf("data_user_del: exec_database error!");
+                return -1;
+        }
+        return 0;
 }
